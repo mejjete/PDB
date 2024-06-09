673f286b32ca64b7e58126ccf77441b9a7322373
Mykyta <nik04ta01@gmail.com>
2024-06-09 19:28:09 +0200

    added basic ui actions

--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "setorremovebreakpointdialog.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
+class QString;
+class QColor;
 }
 QT_END_NAMESPACE
 
@@ -17,7 +20,20 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionOpen_triggered();
+    void on_actionSet_or_Remove_Breakpoint_triggered();
+
+    void setOrRemoveBreakpoint(int lineNumber);
+
 private:
     Ui::MainWindow *ui;
+    SetOrRemoveBreakpointDialog* m_breakpointCfgDialog;
+    bool m_isBreakpointSet;
+
+    void initUI();
+
+    // Helper method.
+    void loadTextFileWithLineNumbers(const QString& filePath);
 };
 #endif // MAINWINDOW_H
