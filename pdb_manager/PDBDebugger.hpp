f63ec00fe7f80d4f51a191bb429923032c986586
mejjete <danil.malapura@gmail.com>
2024-06-21 15:16:52 +0200

    propagated data members from PDBDebugger to its derived implementation classes

--- pdb_manager/PDBDebugger.hpp
+++ pdb_manager/PDBDebugger.hpp
@@ -1,32 +1,18 @@
 #pragma once
 
 #include <string>
-#include <utility>
+#include <list>
 #include <vector>
 
 namespace pdb
 {
     class PDBDebugger
     {
-    protected: 
-        using pdb_br = std::vector<std::pair<std::string, std::vector<int>>>;
-
-        std::string exec_name;
-        std::string exec_opts;
-
-        pdb_br breakpoints; 
     public:
-        /**
-         *  Initializes new debugger
-         * 
-         *  @param name - debugger executable name
-         *  @param opts - debugger specific options
-         */
-        PDBDebugger(std::string name, std::string opts) 
-            : exec_name(name), exec_opts(opts) {};
+        using pdb_br = std::list<std::pair<std::string, std::vector<int>>>;
 
-        std::string getOptions()    { return exec_opts; };
-        std::string getExecutable() { return exec_name; };
+        virtual std::string getOptions() = 0;
+        virtual std::string getExecutable() = 0;
         virtual pdb_br getBreakpoints() = 0;
         virtual std::string getSource() = 0;
         virtual std::vector<std::string> getSourceFiles() = 0;
@@ -36,11 +22,21 @@ namespace pdb
     // GNU gdb interface
     class GDBDebugger : public PDBDebugger
     {
+    private:
+        using PDBDebugger::pdb_br;
+        const std::string exec_name;
+        const std::string exec_opts;
+
+        pdb_br breakpoints;
+
     public:
         // By default, gdb will launch with Machine Interface enabled
         GDBDebugger(std::string name = "/usr/bin/gdb", std::string opts = "") 
-            : PDBDebugger(name, opts + "-q --interpreter=mi2") {};
+            : exec_name(name), exec_opts(opts + " -q --interpreter=mi2") {};
 
+        std::string getOptions() { return exec_opts; };
+        std::string getExecutable() { return exec_name; };
+ 
         virtual pdb_br getBreakpoints() { return breakpoints; };
         virtual std::string getSource() { return "source"; };
         virtual std::vector<std::string> getSourceFiles();
@@ -51,16 +47,22 @@ namespace pdb
     class LLDBDebugger : public PDBDebugger
     {
     private:
-    using PDBDebugger::pdb_br;
+        using PDBDebugger::pdb_br;
+        const std::string exec_name;
+        const std::string exec_opts;
+
+        pdb_br breakpoints;
 
     public:
         LLDBDebugger(std::string name = "/usr/bin/lldb", std::string opts = "") 
-            : PDBDebugger(name, opts) {};
+            : exec_name(name), exec_opts(opts) {};
         
+        std::string getOptions() { return exec_opts; };
+        std::string getExecutable() { return exec_name; };
+
         virtual pdb_br getBreakpoints() { return breakpoints; };
         virtual std::string getSource() { return "source"; };
         virtual std::vector<std::string> getSourceFiles() { return std::vector<std::string>(1, "sourceFiles"); };
         virtual std::string submitComm(std::string comm) { return comm; };
     };
 }
-
