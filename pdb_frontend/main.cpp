2614da6baa930cb10c04b6933bffa5291c324e6c
mejjete <danil.malapura@gmail.com>
2025-08-26 21:01:15 +0200

    Separated front-end from back-end build systems

--- pdb_frontend/main.cpp
+++ pdb_frontend/main.cpp
@@ -1,16 +1,10 @@
 #include "mainwindow.h"
-#include <PDB.hpp>
 #include <QApplication>
 #include <memory>
-
-using Debugger = pdb::PDBDebug<pdb::GDBDebugger>;
 
 int main(int argc, char **argv) {
   QApplication app(argc, argv);
 
-  using namespace pdb;
-  auto debug =
-      Debugger::create("mpirun -np 4", "/usr/bin/gdb", "./mpi_test.out");
   MainWindow mainWindow;
   mainWindow.resize(720, 480);
   mainWindow.setWindowTitle("PDB UI (beta)");
